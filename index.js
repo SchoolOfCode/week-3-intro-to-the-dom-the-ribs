8143b94ce5b39418d0b19e1c86fb40d4cde8f45c
Shanti Rai <shantirai@MacBookAir.lan>
2024-12-09 13:22:57 +0000

    added solutions for  ticket1 to the index.js file as per instructions

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('linked');
+//Ticket 1: selecting DOM elements
+//1a
+console.log(document.getElementById('title'));
+
+//1b
+const content = document.getElementsByClassName('content');
+console.log(content);
+
+//1c
+const liElements = document.getElementsByTagName('li');
+console.log(liElements)
+
+//1d
+const highlight = document.querySelector('.highlight'); //need the dot notaiton to indicate its a class
+console.log(highlight)
+
+//1e
+const allHighlight = document.querySelectorAll('.highlight');
+console.log(allHighlight)
+
+
+//Ticket2: traversing the DOM
+//2a
